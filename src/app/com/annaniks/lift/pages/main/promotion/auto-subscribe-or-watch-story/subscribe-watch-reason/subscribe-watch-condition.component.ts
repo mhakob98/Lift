d39f3cc5bbcc220f4acee012ac60c7048b9f65d4
mhakob98 <mshecyan98@gmail.com>
2020-01-14 16:49:26 +0400

    some minor things

---
+++ src/app/com/annaniks/lift/pages/main/promotion/auto-subscribe-or-watch-story/subscribe-watch-reason/subscribe-watch-condition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribe-watch-condition',
+  templateUrl: './subscribe-watch-condition.component.html',
+  styleUrls: ['./subscribe-watch-condition.component.scss']
+})
+export class SubscribeWatchConditionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
