9b72a7d816c9e1c36cd0507afc9337c95dae0f27
mhakob98 <mshecyan98@gmail.com>
2020-01-22 10:50:22 +0400

    subscribe parametres

--- src/app/com/annaniks/lift/pages/main/promotion/auto-subscribe-or-watch-story/auto-subscribe-watch-story.service.ts
+++ src/app/com/annaniks/lift/pages/main/promotion/auto-subscribe-or-watch-story/auto-subscribe-watch-story.service.ts
@@ -14,6 +14,7 @@ import { AccountSettings } from '../../../../core/models/account';
 
 @Injectable()
 export class AutoSubscribeOrWatchStoryService {
+
     private _settingsEvent$ = new BehaviorSubject<AccountSettings>(new AccountSettings());
     public settings: AccountSettings = new AccountSettings();
 
