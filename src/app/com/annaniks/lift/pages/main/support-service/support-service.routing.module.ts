6118966b48de50ad8bf51ca7b4025b3a63065394
Rado <rado.poghosyan@annaniks.com>
2020-01-31 11:48:09 +0400

    created ticket module

--- src/app/com/annaniks/lift/pages/main/support-service/support-service.routing.module.ts
+++ src/app/com/annaniks/lift/pages/main/support-service/support-service.routing.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Our Components
 import { SupportServiceComponent } from './support-service.component';
-import { AddQuestionComponent } from './add-question/add-question.component';
 
 const supportServiceRoutes: Routes = [
     { path: '', component: SupportServiceComponent }
@@ -15,6 +14,6 @@ const supportServiceRoutes: Routes = [
     exports: [RouterModule]
 })
 export class SupportServiceRoutingModule {
-    static components = [SupportServiceComponent, AddQuestionComponent]
-    static entryComponents = [AddQuestionComponent]
+    static components = [SupportServiceComponent];
+    static entryComponents = [];
 }
