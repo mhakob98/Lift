f621270a2374f8e744d82189f88b795c5ac8c4c5
Rado <rado.poghosyan@annaniks.com>
2020-02-01 09:49:10 +0400

    added affiliate-programm module

---
+++ src/app/com/annaniks/lift/pages/main/affiliate-program/affiliate-program.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AffiliateProgramRoutingModule } from './affiliate-program.routing.module';
+import { AffiliateProgramComponent } from './affiliate-program.component';
+
+@NgModule({
+    declarations: [AffiliateProgramComponent],
+    imports: [AffiliateProgramRoutingModule],
+    providers: []
+})
+export class AffiliateProgramModule { }
