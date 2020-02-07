33407077d224ac76540471e706c54e2cc8d8ff33
mhakob98 <mshecyan98@gmail.com>
2020-02-07 13:04:37 +0400

    rdit

--- src/app/com/annaniks/lift/pages/main/profile/components/additional-settings/additional-settings.component.ts
+++ src/app/com/annaniks/lift/pages/main/profile/components/additional-settings/additional-settings.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProfileService } from '../../profile.service';
+import { takeUntil, finalize } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { ChangeMe } from 'src/app/com/annaniks/lift/core/models/change-me';
 
 @Component({
     selector: "additional-settings",
@@ -7,12 +11,17 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
     styleUrls: ["additional-settings.component.scss"]
 })
 
-export class AdditionalSettings implements OnInit {
+export class AdditionalSettings implements OnInit, OnDestroy {
 
+    private _unsubscribe$: Subject<void> = new Subject<void>();
     public additionalForm: FormGroup;
     public pagesForm: FormGroup;
+    public loading: boolean = false;
 
-    constructor(private _fb: FormBuilder) { }
+    constructor(
+        private _fb: FormBuilder,
+        private _profileService: ProfileService
+    ) { }
 
     ngOnInit() {
         this._formBuilder();
@@ -20,18 +29,49 @@ export class AdditionalSettings implements OnInit {
 
     private _formBuilder(): void {
         this.additionalForm = this._fb.group({
-            service: [""],
-            occupation: [""],
-            activity: [""],
-            description:["",Validators.required]
+            service: [null],
+            occupation: [null],
+            activity: [null],
+            description: ["", Validators.required]
         })
         this.pagesForm = this._fb.group({
             instagram: ["", Validators.required],
             facebook: ["", Validators.required]
         })
+        this.additionalForm.valueChanges.subscribe(data => {
+            console.log(data);
+
+        })
     }
 
-    public checkIsValid(formGroup,controlName): boolean {
+    public checkIsValid(formGroup, controlName): boolean {
         return formGroup.get(controlName).hasError('required') && formGroup.get(controlName).touched;
+    }
+
+    public changeMe(): void {
+        this.loading = true
+        let additionalForm = this.additionalForm.value
+        let pageForm = this.pagesForm.value
+        let sendingData: ChangeMe = {
+            // goalUsing: additionalForm.service,
+            // occupation: additionalForm.occupation || 0,
+            aboutYourself: additionalForm.description,
+            facebookLink: pageForm.facebook,
+            instagramLink: pageForm.instagram
+        }
+        this._profileService.changeMe(sendingData)
+            .pipe(
+                takeUntil(this._unsubscribe$),
+                finalize(() => this.loading = false)
+            ).
+            subscribe((response) => {
+                console.log(response);
+
+            })
+    }
+
+    ngOnDestroy() {
+        this._unsubscribe$.next();
+        this._unsubscribe$.unsubscribe();
     }
 }
--- src/app/com/annaniks/lift/pages/main/profile/profile.service.ts
+++ src/app/com/annaniks/lift/pages/main/profile/profile.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ChangePasswordData } from '../../../core/models/account-basic-settings';
+import { ChangeMe } from '../../../core/models/change-me';
 
 @Injectable()
 export class ProfileService {
@@ -9,7 +10,10 @@ export class ProfileService {
     constructor(private _httpClient: HttpClient) { }
 
     public changePassword(data: ChangePasswordData): Observable<any> {
-
         return this._httpClient.post('change-password', data);
+    }
+
+    public changeMe(data: ChangeMe): Observable<any> {
+        return this._httpClient.put('me', data)
     }
 }
