d11b23b26166247b2674842db77a8d66944c2cd9
Rado <rado.poghosyan@annaniks.com>
2020-04-14 12:17:29 +0400

    more changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://176.118.165.95:18180',
+  apiUrl: 'http://176.118.165.95:18180/',
   fileUrl: 'http://176.118.165.95:18180/files',
-  socketUrl: 'http://176.118.165.95:18180/direct'
+  socketUrl: 'http://176.118.165.95:3031/direct'
 };
